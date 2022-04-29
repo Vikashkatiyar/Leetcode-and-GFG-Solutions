fa90a0aecdf0eb7040f1e8cafbbf8519943e2c79
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-04-29 21:28:10 +0530

    Update 2185-counting-words-with-a-given-prefix.java

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -15,3 +15,23 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+//second method
+
+
+// class Solution {
+//     public int prefixCount(String[] words, String pref) {
+        
+//         int res = 0; 
+//         for(int i = 0; i<words.length; i++){
+//             if(words[i].startsWith(pref)){
+//                 //startsWith()method applicable on String in java
+//                res++; 
+//             }
+//         }
+//         return res; 
+//     }
+// }
