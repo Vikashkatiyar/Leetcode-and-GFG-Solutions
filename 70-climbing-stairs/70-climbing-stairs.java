194e3936d7612485b1951ed4243fbc24bfb1439f
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-05-12 16:31:31 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (59.69%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        //its like fibonnaci program in dynamic programming
+        //tabulation approach
+        
+        if(n<=1){
+            return 1;
+        }
+        
+        int []dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+    }
+}
