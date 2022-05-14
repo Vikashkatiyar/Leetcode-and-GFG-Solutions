ae2351df93860e727b20b5eb6a9041ed9b3d0fa6
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-05-14 07:40:08 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (9.07%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -14,34 +14,62 @@
  * }
  */
 class Solution {
-    
-    public static void inorder(TreeNode root,ArrayList<Integer> list){
-       if(root==null){
-           return;
-       }
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode result = new TreeNode();
+        dfs(root,result);
+        return result.right;
     }
     
+    public TreeNode dfs(TreeNode root, TreeNode result){
+        if (root == null) return result;
+        result = dfs(root.left,result);
+        result.right = new TreeNode(root.val);
+        result = dfs(root.right,result.right);
+        return result;
+    }
     
-    public TreeNode increasingBST(TreeNode root) {
-        ArrayList<Integer> list=new ArrayList<>();
-        inorder( root,list);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public static void inorder(TreeNode root,ArrayList<Integer> list){
+//        if(root==null){
+//            return;
+//        }
+//         inorder(root.left,list);
+//         list.add(root.val);
+//         inorder(root.right,list);
+//     }
+    
+    
+//     public TreeNode increasingBST(TreeNode root) {
+//         ArrayList<Integer> list=new ArrayList<>();
+//         inorder( root,list);
         
-        if(list.size()==0){
-             return null;
-        }
-       TreeNode temp=new TreeNode(list.get(0));
-        root=temp;
-        for(int i=1;i<list.size();i++){
-            TreeNode curr=new TreeNode(list.get(i));
-            temp.right=curr;
-            temp.left=null;
-            temp=temp.right;
+//         if(list.size()==0){
+//              return null;
+//         }
+//        TreeNode temp=new TreeNode(list.get(0));
+//         root=temp;
+//         for(int i=1;i<list.size();i++){
+//             TreeNode curr=new TreeNode(list.get(i));
+//             temp.right=curr;
+//             temp.left=null;
+//             temp=temp.right;
             
              
-        }
-        return root;
-    }
-}
+//         }
+//         return root;
+//     }
