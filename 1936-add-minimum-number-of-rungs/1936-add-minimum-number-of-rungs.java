a7fe9e3dd764ab94b4d0405b1666815fbe9d2a84
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-06-03 00:11:49 +0530

    Time: 2 ms (76.65%), Space: 54.4 MB (80.71%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.java
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.java
@@ -1,9 +1,18 @@
 class Solution {
     public int addRungs(int[] rungs, int dist) {
-         int res = (rungs[0] - 1) / dist;
-        // int res=0;
-    for (int i = 1; i < rungs.length; ++i)
-        res += (rungs[i] - rungs[i - 1] - 1) / dist;
-    return res;
+    //      int res = (rungs[0] - 1) / dist;
+    // for (int i = 1; i < rungs.length; ++i)
+    //     res += (rungs[i] - rungs[i - 1] - 1) / dist;
+    // return res;
+        
+        
+   //method 2     
+        int prev=0;
+        int res=0;
+        for(int rung:rungs){
+            res+=(rung-prev-1)/dist;
+            prev=rung;
+        }
+        return res;
     }
 }
