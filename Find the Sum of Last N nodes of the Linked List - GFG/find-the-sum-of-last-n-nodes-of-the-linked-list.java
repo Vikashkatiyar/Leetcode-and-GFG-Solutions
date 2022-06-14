77480de9ec8e77fc6031f371c6c371c2e9358b61
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-06-14 11:31:04 +0530

    Added solution - LeetHub

---
+++ Find the Sum of Last N nodes of the Linked List - GFG/find-the-sum-of-last-n-nodes-of-the-linked-list.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.sum(head, k));
+		}
+	}
+}// } Driver Code Ends
+
+
+/*
+Node class is as follows:
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    
+    //Return the sum of last k nodes
+    public int sum(Node head, int k){
+      Node temp=head;
+      int len=0;
+      while(temp!=null){
+          temp=temp.next;
+          len++;
+      }
+      temp=head;
+      for(int i=0;i<len-k;i++){
+          temp=temp.next;
+      }
+      int sum=0;
+      
+      while(temp!=null){
+          sum+=temp.data;
+          temp=temp.next;
+      }
+      return sum;
+    }
+}
