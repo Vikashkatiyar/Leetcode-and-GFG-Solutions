7bf80e866f23fabcdb1b7eb344cb2909e5358a06
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-06-17 01:25:21 +0530

    Added solution - LeetHub

---
+++ Rotating an Array - GFG/rotating-an-array.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int d = Integer.parseInt(br.readLine().trim());
+
+            new Solution().leftRotate(arr, n, d);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+   public void reverse(int[] arr, int start, int end )
+   {
+       int i= start, j= end;
+       
+       while(i<j)
+       {
+           int temp = arr[i];
+           arr[i]= arr[j];
+           arr[j]= temp;
+           
+           i++;
+           j--;
+       }
+   }
+   void leftRotate(int[] arr, int n, int d) {
+       int start = 0, end= n-1;
+       
+       reverse(arr, start, end);
+       reverse(arr, start, (n-d-1));
+       reverse(arr, (n-d), end); 
+   }
+}
