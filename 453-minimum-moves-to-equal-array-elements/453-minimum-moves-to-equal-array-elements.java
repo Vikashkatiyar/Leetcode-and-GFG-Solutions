77a5d362801edead6df3059bd2931a4d54e6098f
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-06-30 21:51:55 +0530

    Time: 15 ms (23.39%), Space: 54.3 MB (44.35%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -1,15 +1,30 @@
 class Solution {
     // TC:O(n)
     // SC:O(1)
+    // public int minMoves(int[] nums) {
+    //     int min=Integer.MAX_VALUE;
+    //     for(int ele:nums){
+    //         min=Math.min(ele,min);
+    //     }
+    //     int count=0;
+    //     for(int ele:nums){
+    //         count+=Math.abs(min-ele);
+    //     }
+    //     return count;
+    // }
+    
+    
+    
+    
+    //TC:(nlogn)
     public int minMoves(int[] nums) {
-        int min=Integer.MAX_VALUE;
-        for(int ele:nums){
-            min=Math.min(ele,min);
-        }
+        Arrays.sort(nums);
+        
         int count=0;
         for(int ele:nums){
-            count+=Math.abs(min-ele);
+            count+=Math.abs(ele-nums[0]);
         }
         return count;
     }
+    
 }
