41e97e2ad8ad9352e8df9888ca4255dbba3e1b68
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-08-14 01:05:13 +0530

    Time: 2 ms (90.06%), Space: 54.7 MB (14.04%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -1,30 +1,31 @@
 class Solution {
-    // TC:O(n)
-    // SC:O(1)
-    // public int minMoves(int[] nums) {
-    //     int min=Integer.MAX_VALUE;
-    //     for(int ele:nums){
-    //         min=Math.min(ele,min);
-    //     }
-    //     int count=0;
-    //     for(int ele:nums){
-    //         count+=Math.abs(min-ele);
-    //     }
-    //     return count;
-    // }
-    
-    
-    
-    
-    //TC:(nlogn)
+    //TC:O(n)
+    //SC:O(1)
     public int minMoves(int[] nums) {
-        Arrays.sort(nums);
-        
-        int count=0;
-        for(int ele:nums){
-            count+=Math.abs(ele-nums[0]);
+      int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            min=Math.min(nums[i],min);
         }
-        return count;
+        
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+          res+=nums[i]-min;
+        }
+        return res;
     }
+    
+    
+    
+    
+//     //TC:(nlogn)
+//     public int minMoves(int[] nums) {
+//         Arrays.sort(nums);
+        
+//         int count=0;
+//         for(int ele:nums){
+//             count+=Math.abs(ele-nums[0]);
+//         }
+//         return count;
+//     }
     
 }
