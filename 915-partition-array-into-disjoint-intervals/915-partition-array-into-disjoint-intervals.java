a8c45d7a7a191cabfd4a439eb6fc4ecd31d99472
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-08-17 12:23:15 +0530

    Time: 3 ms (93.31%), Space: 86.8 MB (87.21%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,35 +1,57 @@
 class Solution {
     //O(n)
-    //O(n);
+    //O(1)
     public int partitionDisjoint(int[] nums) {
-        int []left=new int[nums.length];
-        int []right=new int[nums.length];
+        int pi=0;
         
+        int mtp=nums[0];
+        int mts=nums[0];
         
-        for(int i=0;i<nums.length;i++){//fill with max numbers
-            if(i==0)
-                left[i]=nums[i];
-            else
-                left[i]=Math.max(nums[i],left[i-1]);
-        }
-        
-        
-        for(int i=nums.length-1;i>=0;i--){//fill with minimum numbers
-            if(i==nums.length-1)
-                right[i]=nums[i];
-            else
-                right[i]=Math.min(nums[i],right[i+1]);
-        }
-        
-        
-        int ans=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(left[i]<=right[i+1]){
-                ans=i+1;
-                break;
+        for(int i=1;i<nums.length;i++){
+            mts=Math.max(mts,nums[i]);
+            
+            if(nums[i]<mtp){
+                pi=i;
+                mtp=mts;
             }
         }
-        
-        return ans;
+       return pi+1; 
     }
 }
+
+
+
+
+// //O(n)
+//     //O(n);
+//     public int partitionDisjoint(int[] nums) {
+//         int []left=new int[nums.length];
+//         int []right=new int[nums.length];
+        
+        
+//         for(int i=0;i<nums.length;i++){//fill with max numbers
+//             if(i==0)
+//                 left[i]=nums[i];
+//             else
+//                 left[i]=Math.max(nums[i],left[i-1]);
+//         }
+        
+        
+//         for(int i=nums.length-1;i>=0;i--){//fill with minimum numbers
+//             if(i==nums.length-1)
+//                 right[i]=nums[i];
+//             else
+//                 right[i]=Math.min(nums[i],right[i+1]);
+//         }
+        
+        
+//         int ans=0;
+//         for(int i=0;i<nums.length-1;i++){
+//             if(left[i]<=right[i+1]){
+//                 ans=i+1;
+//                 break;
+//             }
+//         }
+        
+//         return ans;
+//     }
