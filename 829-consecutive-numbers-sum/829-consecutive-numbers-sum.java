0ff1fb8deb5c9aebbe9e9efa14b4fba5752547cb
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-08-18 07:53:40 +0530

    Time: 5 ms (88.37%), Space: 39 MB (97.88%) - LeetHub

---
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    //O(root of n)
+    public int consecutiveNumbersSum(int n) {
+        int res=0;
+        int k=1;
+        int check=k*(k-1)/2;
+        while(check<n){
+          if((n-check)%k==0){
+            res++;
+          }
+          k++;
+            check=k*(k-1)/2;
+        }
+         return res;
+    }
+}
