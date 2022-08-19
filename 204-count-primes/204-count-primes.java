2cef296325c2938f3d12413eb07cdcc4a541b3c8
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-08-19 08:38:14 +0530

    Time: 136 ms (85.65%), Space: 68.6 MB (40.08%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,31 @@
+class Solution {
+    //TC:O(nlogn)
+    public int countPrimes(int n) {
+        if(n<2){
+            return 0;
+        }
+        
+        boolean []primes=new boolean[n+1];
+        Arrays.fill(primes,true);
+        primes[0]=false;
+        primes[1]=false;
+        
+        for(int tbl=2;tbl*tbl<=n;tbl++){
+              if(primes[tbl]==false){
+                  continue;
+              }
+              for(int j=tbl;j*tbl<=n;j++){
+                 primes[j*tbl]=false;
+              }
+            
+        }
+        
+        int count=0;
+        for(int i=0;i<primes.length-1;i++){
+            if(primes[i]==true){
+                count++;
+            }
+        }
+        return count;
+    }
+}
