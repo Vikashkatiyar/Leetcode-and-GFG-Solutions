503fd7305111682dcf75badfded50bb329ad2ba4
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-09-16 09:38:39 +0530

    Time: 30 ms (38.74%), Space: 54.3 MB (46.85%) - LeetHub

---
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.java
@@ -0,0 +1,42 @@
+class Solution {
+    //Z Algorithm -> KMP Algorithum
+    //TC:O(n+m)
+    //Revise again
+    public long sumScores(String s) {
+        long ans = s.length();
+        
+        int[] z = new int[s.length()];
+        int l = 0;
+        int r = 0;
+        for(int i = 1; i < z.length; i++){
+            if(i > r){
+               l = r = i;
+                while(r < z.length && s.charAt(r) == s.charAt(r - l)){
+                    r++;
+                }
+                
+                r--;
+                z[i] = r - l + 1;
+            } else {
+                int k = i - l;
+                if(z[k] < r - i + 1){
+                    z[i] = z[k];
+                } else {
+                    l = i;
+                    while(r < z.length && s.charAt(r) == s.charAt(r - l)){
+                        r++;
+                    }
+
+                    r--;
+                    z[i] = r - l + 1; 
+                }
+            }
+        }
+        
+        for(int val: z){
+            ans += val;
+        }
+        
+        return ans;
+    }
+}
