ee4f2195c32a12680519cad73b41276054650252
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-09-17 23:28:58 +0530

    Time: 3 ms (84.60%), Space: 49.1 MB (83.31%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,51 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-      //Bayers Moore Voting Algorithum
-        //O(N)+O(N)
-        //o(1)
-        int num1=-1;
-        int num2=-1;
+        int cand1=0;
         int count1=0;
+        
+        int cand2=0;
         int count2=0;
         
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==num1)
+        for(int num:nums){
+            if(num==cand1)   
                 count1++;
-            else if(nums[i]==num2)
+            else if(num==cand2)
                 count2++;
             else if(count1==0){
-                num1=nums[i];
-                count1=1;
+                count1++;
+                cand1=num;
             }
             else if(count2==0){
-                num2=nums[i];
-                count2=1;
-            }
-            else{
+                count2++;
+                cand2=num;
+            }else{
                 count1--;
                 count2--;
             }
         }
-            
-            ArrayList<Integer> list=new ArrayList<>();
-            count1=0;
-            count2=0;
-            
-            for(int num:nums){
-                if(num==num1)
-                    count1++;
-                else if(num==num2)
-                    count2++;
-            }
-            
-            if(count1>nums.length/3){
-                list.add(num1);
-            }
-            if(count2>nums.length/3){
-                list.add(num2);
-            }
-         
         
-         return list;
+        
+        count1=0;
+        count2=0;
+        ArrayList<Integer> list=new ArrayList<>();
+         for(int val:nums){   //cross checking 
+             if(val==cand1){
+                 count1++;
+             }else if(val==cand2){
+                 count2++;
+             }
+         }
+        
+        if(count1>nums.length/3){
+            list.add(cand1);
+        }
+        if(count2>nums.length/3){
+            list.add(cand2);
+        }
+        return list;
     }
 }
