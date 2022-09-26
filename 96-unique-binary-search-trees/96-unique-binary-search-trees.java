e2d49d2b9260de9ca6b75faa4e8e2a215d57e47e
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-09-26 11:01:27 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (6.68%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,18 @@
+class Solution {
+    //catalon group
+    public int numTrees(int n) {
+        int []dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int left=0;
+            int right=i-1;
+            while(left<=i-1){
+                dp[i]+=dp[left]*dp[right];
+                left++;
+                right--;
+            }
+        }
+        return dp[n];
+    }
+}
