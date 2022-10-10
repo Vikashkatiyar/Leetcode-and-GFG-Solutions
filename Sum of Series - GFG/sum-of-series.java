1722cd1f98a4ff9d102af230c988ed80a9eab4bc
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-10-10 23:56:29 +0530

    Added solution - LeetHub

---
+++ Sum of Series - GFG/sum-of-series.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            long ans = new Solution().seriesSum(n);
+
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // function to return sum of  1, 2, ... n
+    long seriesSum(int n) {
+        return (long)n*(n+1)/2;
+    }
+}
