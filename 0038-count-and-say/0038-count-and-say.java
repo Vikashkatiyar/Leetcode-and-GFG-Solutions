0700dff6d32bb56ea735566ab6c767e511863a4a
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-10-18 14:12:30 +0530

    Time: 4 ms (85.69%), Space: 41.7 MB (75.63%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s="1";
+        for(int i=1;i<n;i++){
+            s=countAndSayHelp(s);
+        }
+        return s;
+    }
+    public String countAndSayHelp(String s){
+        StringBuilder sb=new StringBuilder();
+        char previous=s.charAt(0);
+        int count=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==previous){
+                count++;
+            }else{
+                sb.append(count);
+                sb.append(previous);
+                previous=s.charAt(i);
+                count=1;
+            }
+        }
+        sb.append(count);
+        sb.append(previous);
+        return sb.toString();
+    }
+}
