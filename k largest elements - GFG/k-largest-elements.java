28067cafc10c07a0e0f417611b4f10c8be7ce3ba
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-11-04 22:40:19 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        int[] res=new int[k];
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int num:arr){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        int i=k-1;
+        while(!pq.isEmpty()){
+            res[i]=pq.remove();
+            i--;
+        }
+        return res;
+    }
+}
