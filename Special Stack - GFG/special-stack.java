318036ae83271f3022b0fe8b7f4ba162fded464d
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-11-10 16:15:00 +0530

    Added solution - LeetHub

---
+++ Special Stack - GFG/special-stack.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.Stack;
+class SpeStack{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			Stack<Integer> s=new Stack<>();
+			GfG g=new GfG();
+			while(!g.isEmpty(s)){
+			    g.pop(s);
+			}
+			while(!g.isFull(s,n)){
+				g.push(sc.nextInt(),s);
+			}
+		System.out.println(g.min(s));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function(s) below*/
+class GfG{
+	public void push(int a,Stack<Integer> s)
+	{
+	    s.push(a);
+	}
+	public int pop(Stack<Integer> s)
+        {
+            return s.pop();
+	}
+	public int min(Stack<Integer> s)
+        {
+           int min=Integer.MAX_VALUE;
+           for(int num:s){
+               min=Math.min(min,num);
+           }
+           return min;
+	  }
+	public boolean isFull(Stack<Integer>s, int n)
+        {
+           //add code here.
+           return s.size()==n;
+	}
+	public boolean isEmpty(Stack<Integer>s)
+        {
+           //add code here.
+           return s.size()==0;
+	}
+}
