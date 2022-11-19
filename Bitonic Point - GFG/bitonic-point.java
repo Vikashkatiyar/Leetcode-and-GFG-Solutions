0445d896ad0e66bf54b119b49bca0eb162b049be
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-11-19 13:54:58 +0530

    Added solution - LeetHub

---
+++ Bitonic Point - GFG/bitonic-point.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return arr[mid];
+            }else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return arr[left];
+    }
+}
