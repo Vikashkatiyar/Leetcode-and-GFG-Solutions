3d9a6e98594f13d1f4472e9f986cd6cbdcf749f1
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-04 21:16:48 +0530

    Time: 64 ms (5.24%), Space: 71.6 MB (81.66%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int n=nums.length;
+        long leftSum=0;
+        long rightSum = Arrays.stream(nums).asLongStream().sum();
+        
+        int minAvg=Integer.MAX_VALUE;
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            leftSum+=nums[i];
+            rightSum-=nums[i];
+            
+            int currDiff = (int)(Math.abs(leftSum / (i + 1) - rightSum / Math.max((n - (i + 1)), 1) ));
+            if(currDiff<minAvg){
+                minAvg=currDiff;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
