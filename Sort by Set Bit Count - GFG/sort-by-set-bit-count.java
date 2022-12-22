c166ae59854b81de93de765dfe8b1e98a4278887
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-22 20:38:07 +0530

    Added solution - LeetHub

---
+++ Sort by Set Bit Count - GFG/sort-by-set-bit-count.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Integer arr[] = new Integer[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.sortBySetBitCount(arr, n);
+            StringBuilder output = new StringBuilder();
+            for(int i = 0; i < n; i++)
+            output.append(arr[i] + " ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute  
+{ 
+    static void sortBySetBitCount(Integer arr[], int n)
+    { 
+        Arrays.sort(arr,(a,b) -> Integer.bitCount(b) - Integer.bitCount(a)); 
+    } 
+}
