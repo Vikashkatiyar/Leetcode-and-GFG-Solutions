a3376bc788f1e6b0b4edf1b57bbf2a0bc43bbe79
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-24 02:29:44 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (80.39%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,18 @@
+class Solution {
+    //same as 714. Best Time to Buy and Sell Stock with Transaction Fee
+    public int maxProfit(int[] prices) {
+        int obsp=-prices[0];
+        int ossp=0;
+        int ocsp=0;
+        for(int i=1;i<prices.length;i++){
+            int nbsp=Math.max(ocsp-prices[i],obsp);
+            int nssp=Math.max(obsp+prices[i],ossp);
+            int ncsp=Math.max(ossp,ocsp);
+            
+            obsp=nbsp;
+            ossp=nssp;
+            ocsp=ncsp;
+        }
+        return ossp;
+    }
+}
