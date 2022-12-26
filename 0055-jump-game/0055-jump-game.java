720cb0c4b7f4e05f3394885730abd2a0d8d2203d
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-26 15:14:08 +0530

    Time: 2 ms (92.42%), Space: 42.5 MB (98.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        //valley peak approach
+        //https://www.youtube.com/watch?v=muDPTDrpS28 
+        //O(n),O(1)
         int reacheble=0;
         for(int i=0;i<nums.length;i++){
             if(reacheble<i){ //this handle the Zero part 
