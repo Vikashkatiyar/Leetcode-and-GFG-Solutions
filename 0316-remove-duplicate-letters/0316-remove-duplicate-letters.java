0a8626327cd3394301db8fdc96f5f66e193d750d
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-28 13:17:37 +0530

    Time: 3 ms (88.54%), Space: 42.2 MB (83.87%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,29 +1,29 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         char []arr=s.toCharArray();
-        int[] rem=new int[26];
+        int []freq=new int[26];
         for(char ch:arr){
-            rem[ch-'a']++;
+            freq[ch-'a']++;
         }
         
-        boolean[] ais=new boolean[26];
+        boolean []check=new boolean[26];
         Stack<Character> st=new Stack<>();
         for(char ch:arr){
-            rem[ch-'a']--;
+            freq[ch-'a']--;
             
-            if(ais[ch-'a']==false){
-                while(st.size()>0 && rem[st.peek()-'a']>0 && ch<st.peek()){
-                    ais[st.peek()-'a']=false;
+            if(check[ch-'a']==false){
+                while(st.size()>0 && freq[st.peek()-'a']>0 && ch<st.peek()){
+                    check[st.peek()-'a']=false;
                     st.pop();
                 }
                 
                 st.push(ch);
-                ais[ch-'a']=true;
+                check[ch-'a']=true;
             }
         }
         
         StringBuilder sb=new StringBuilder();
-        while(st.size()!=0){
+        while(!st.isEmpty()){
             sb.append(st.peek());
             st.pop();
         }
