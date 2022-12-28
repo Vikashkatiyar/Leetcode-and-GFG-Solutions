0ddf2b721108b55ab68519b6cf70f54600d6419d
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-28 14:11:51 +0530

    Time: 506 ms (16.21%), Space: 53.7 MB (78.57%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPossibleDivide(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int num:nums){
+            pq.offer(num);
+        }
+        
+        while(!pq.isEmpty()){
+            int ele=pq.peek();
+            for(int i=0;i<k;i++){
+                if(pq.remove(ele+i)==false){
+                    //The remove method returns true if an object passed as a parameter is removed from the list. Otherwise, it returns false
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
