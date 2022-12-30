ba44ac6f5250533f783e5ea4e7ad0f2dc2d3c7a7
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2022-12-30 22:41:41 +0530

    Added solution - LeetHub

---
+++ Single valued subtree - GFG/single-valued-subtree.java
@@ -0,0 +1,161 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution g = new Solution();
+                System.out.println(g.singlevalued(root));
+                    
+                t--;
+            
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+
+class Solution
+{
+    int ans=0;
+    public int singlevalued(Node root)
+    {
+        ans=0;
+        dfs(root);
+        return ans;
+    }
+    Integer dfs(Node root){ //use Integer for returning the null
+        if(root==null){
+            return null;
+        }
+        
+        Integer lefty=dfs(root.left);
+        Integer righty=dfs(root.right);
+        
+        if(root.left==null && root.right==null){ //for leaf nodes
+            ans++;
+            return root.data;
+        }else if(lefty!=null && righty!=null && lefty==righty && lefty==root.data){ 
+            ans++;
+            return root.data;
+        }else if(root.left==null && righty!=null && righty==root.data){ //left tree is not exist
+            ans++;
+            return root.data;
+        }else if(root.right==null && lefty!=null && lefty==root.data){ //right tree is not exist
+            ans++;
+            return root.data;
+        }
+        return null;
+    }
+}
