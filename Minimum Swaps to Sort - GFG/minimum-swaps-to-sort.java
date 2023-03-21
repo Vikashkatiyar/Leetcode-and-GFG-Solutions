cb35774123818f9be6367fbadc01231c7c26d4d3
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-03-22 01:31:37 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -29,36 +29,47 @@ class GFG
 
 class Solution
 {
+    //O(nlogn)
     //Function to find the minimum number of swaps required to sort the array.
+    
+    class Pair implements Comparable<Pair>{
+        int val;
+        int idx;
+        
+        Pair(int val,int idx){
+            this.val=val;
+            this.idx=idx;
+        }
+        
+        public int compareTo(Pair o){
+            return this.val-o.val;
+        }
+        
+    }
     public int minSwaps(int nums[])
     {
-        // Code here
-        int n=nums.length;
-        int []temp=new int[n];
-        HashMap<Integer,Integer> map=new HashMap<>();
+        Pair[] arr=new Pair[nums.length];
         for(int i=0;i<nums.length;i++){
-            temp[i]=nums[i];
-            map.put(nums[i],i);
+            arr[i]=new Pair(nums[i],i);
         }
-        Arrays.sort(temp);
-        
-        //convert nums array -> temp array
-        int count=0;
-        for(int i=0;i<n;i++){
-              if(temp[i]!=nums[i]){
-                  count++;
-                  int idx=map.get(temp[i]);
-                  swap(nums,i,idx);
-                  map.put(nums[i],i);
-                  map.put(nums[idx],idx);
-              }
+        boolean[] vis=new boolean[nums.length];
+        Arrays.sort(arr);
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(vis[i]==true || arr[i].idx==i){
+                continue;
+            }
+            
+            int clen=0;
+            int j=i;
+            while(vis[j]==false){
+                vis[j]=true;
+                clen++;
+                j=arr[j].idx;
+            }
+            
+            ans+=(clen-1);
         }
-        return count;
-    }
-    
-    static void swap(int []arr,int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+        return ans;
     }
 }
