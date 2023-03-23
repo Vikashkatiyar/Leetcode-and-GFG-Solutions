7dcc1d5bcc3ce5ab21a0131a061358fb252f8d27
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-03-24 00:08:08 +0530

    Time: 7 ms (38.62%), Space: 43.5 MB (21.70%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canFinish(int num, int[][] preq) {
+        
+        //Make directed Graph
+        List<List<Integer>> gdList=new ArrayList<>();
+        for(int i=0;i<num;i++){
+            gdList.add(new ArrayList<>());
+        }
+        for(int i=0;i<preq.length;i++){
+            gdList.get(preq[i][1]).add(preq[i][0]);
+        }
+        
+        
+        //Make inDegree array
+        int[] inDegree=new int[num];
+        for(int i=0;i<num;i++){
+            for(int nbr:gdList.get(i)){
+                inDegree[nbr]++;
+            }
+        }
+        int[] ans=new int[num];
+        
+        LinkedList<Integer> q=new LinkedList<>();
+        for(int i=0;i<num;i++){
+            if(inDegree[i]==0){
+                    q.addLast(i);
+                }
+        }
+        
+        int idx=0;
+        while(q.size()>0){
+            int rem=q.removeFirst();
+            
+            ans[idx++]=rem;
+            
+            for(int nbr:gdList.get(rem)){
+                inDegree[nbr]--;
+                if(inDegree[nbr]==0){
+                    q.addLast(nbr);
+                }
+            }
+        }
+        
+        
+        if(idx==num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
