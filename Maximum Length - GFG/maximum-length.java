01516fad8b0b2d453f25daae1fa297028026132c
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-04-11 22:37:49 +0530

    Added solution - LeetHub

--- Maximum Length - GFG/maximum-length.java
+++ Maximum Length - GFG/maximum-length.java
@@ -54,27 +54,35 @@ class GFG {
 
 class Solution {
     int solve(int a, int b, int c) {
-        if(a>=b && a>=c){
-            int half_a=(a%2==0?a/2:a/2+1);
-            int req=half_a-1;
-            if(b+c>=req){
-                return a+b+c;
-            }
-        }
-        if(b>=a && b>=c){
-          int half_b=(b%2==0?b/2:b/2+1);
-            int req=half_b-1;
-            if(a+c>=req){
-                return a+b+c;
-            }  
-        }
+        // if(a>=b && a>=c){
+        //     int half_a=(a%2==0?a/2:a/2+1);
+        //     int req=half_a-1;
+        //     if(b+c>=req){
+        //         return a+b+c;
+        //     }
+        // }
+        // if(b>=a && b>=c){
+        //   int half_b=(b%2==0?b/2:b/2+1);
+        //     int req=half_b-1;
+        //     if(a+c>=req){
+        //         return a+b+c;
+        //     }  
+        // }
         
-        if(c>=a && c>=b){
-           int half_c=(c%2==0?c/2:c/2+1);
-            int req=half_c-1;
-            if(b+a>=req){
-                return a+b+c;
-            } 
+        // if(c>=a && c>=b){
+        //   int half_c=(c%2==0?c/2:c/2+1);
+        //     int req=half_c-1;
+        //     if(b+a>=req){
+        //         return a+b+c;
+        //     } 
+        // }
+        
+        int sum=a+b+c;
+        int max=Math.max(a,Math.max(b,c));
+        int maxHalf=(max%2==0)?max/2:max/2+1;
+        int req=maxHalf-1;
+        if(sum-max>=req){
+            return sum;
         }
         return -1;
     }
