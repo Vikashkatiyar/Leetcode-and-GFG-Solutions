3c6bb4ce23ad74f34c93eddd337c3540e7c302b5
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-06-01 14:01:07 +0530

    Added solution - LeetHub

---
+++ Odd or Even - GFG/odd-or-even.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.oddEven(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String oddEven(int N){
+        if((N&1)==0){
+            return "even";
+        }else{
+            return "odd";
+        }
+    }
+}
