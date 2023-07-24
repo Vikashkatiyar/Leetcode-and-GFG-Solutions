b2490b76ae144067b24bedbb9b4928d03c895c50
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-07-24 10:30:26 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis=new boolean[V];
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        
+        // 0th vertex from left to right according to the graph.
+        helper(V,adj,0,ans,vis);
+        return ans;
+    }
+    
+    public void helper(int v,ArrayList<ArrayList<Integer>> adj,int parent,ArrayList<Integer>ans,boolean[] vis){
+        
+        vis[parent]=true;
+        ans.add(parent);
+        for(int nbr : adj.get(parent)){
+            if(vis[nbr]==false){
+                helper(v,adj,nbr,ans,vis);
+            }
+        }
+        
+        
+    }
+}
