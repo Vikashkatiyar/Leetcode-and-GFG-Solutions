0bbec3c2077ec1c9ff2d72b100ae0d7027fa15d8
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-09-22 09:29:39 +0530

    Time: 5 ms (98.91%), Space: 58.5 MB (68.46%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minLengthAfterRemovals(List<Integer> nums) {
+           int len = nums.size();
+           int ans = nums.size(), i = 0, j = (len + 1) / 2;
+           while(i < len/ 2 && j < len) {
+           if (nums.get(i) < nums.get(j)) {
+               ans-=2;
+           }
+          i++;
+          j++;
+       }
+      return ans;
+      }
+}
