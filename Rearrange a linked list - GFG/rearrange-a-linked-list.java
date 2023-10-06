c042e968520681b9ca2f92dbc6c0e5824e19ff32
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-10-06 21:01:51 +0530

    Added solution - LeetHub

---
+++ Rearrange a linked list - GFG/rearrange-a-linked-list.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            new Solution().rearrangeEvenOdd(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+This is method only submission.
+You only need to complete the below method.
+*/
+class Solution
+{
+
+   // Should rearrange given linked list such that all even
+   // positioned Nodes are before odd positioned.
+   // Returns nothing
+    void rearrangeEvenOdd(Node head)
+    {
+          Node odd=head;
+          Node even=head.next;
+          Node heven=even;
+          
+          while(even!=null && even.next!=null){
+              odd.next=odd.next.next;
+              even.next=even.next.next;
+              
+              odd=odd.next;
+              even=even.next;
+          }
+          
+          odd.next=heven;
+     }
+}
